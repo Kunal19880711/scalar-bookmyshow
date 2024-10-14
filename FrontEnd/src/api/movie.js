a468b88804656d7440b4616a964feea5d205e855
Kunal19880711 <kunal21634267+github@gmail.com>
2024-10-14 14:39:14 +0530

    [FrontEnd]: Add Movie Table and Forms
    Adding Movie Table and forms to
    - Create movie
    - update movie
    - delete movie

--- FrontEnd/src/api/movie.js
+++ FrontEnd/src/api/movie.js
@@ -1,11 +1,42 @@
-import {axiosInstance} from "./index";
+import { axiosInstance } from "./index";
 
 export const GetAllMovies = async () => {
-    try {
-        const response = await axiosInstance.get("/movies/getAllMovies");
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+  try {
+    const response = await axiosInstance.get("/movies/getAllMovies");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateMovie = async (payload) => {
+  try {
+    const response = await axiosInstance.patch("/movies/updateMovie", payload);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const addMovie = async (payload) => {
+  try {
+    const response = await axiosInstance.post("/movies/addMovie", payload);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+
+export const deleteMovie = async (movieId) => {
+  try {
+    const response = await axiosInstance.delete(`/movies/deleteMovie/${movieId}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
--- FrontEnd/src/pages/Admin/MovieList.jsx
+++ FrontEnd/src/pages/Admin/MovieList.jsx
@@ -1,68 +1,28 @@
-import React, { useEffect } from "react";
-import { DateTime } from "luxon";
-import { Table, message } from "antd";
-
-import { showLoading, hideLoading } from "../../redux/loaderSlice";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { GetAllMovies } from "../../api/movie";
+import moment from "moment";
+import { Button, Table, message } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { showLoading, hideLoading } from "../../redux/loaderSlice";
 
-const tableHeading = [
-  {
-    title: "Poster",
-    dataIndex: "poster",
-    key: "poster",
-    render: (text, data) => (
-      <img
-        width="75"
-        height="115"
-        style={{ objectFit: "cover" }}
-        src={data?.poster}
-        alt="poster"
-      />
-    ),
-  },
-  {
-    title: "Movie Name",
-    dataIndex: "movieName",
-    key: "movieName",
-  },
-  {
-    title: "Description",
-    dataIndex: "description",
-    key: "description",
-  },
-  {
-    title: "Duration",
-    dataIndex: "duration",
-    key: "duration",
-    render: (text) => `${text} min`,
-  },
-  {
-    title: "Genre",
-    dataIndex: "genre",
-    key: "genre",
-  },
-  {
-    title: "Language",
-    dataIndex: "language",
-    key: "language",
-  },
-  {
-    title: "Release Date",
-    dataIndex: "releaseDate",
-    key: "releaseDate",
-    render: (text) => DateTime.fromISO(text).toFormat("dd-MM-yyyy"),
-  },
-];
+import { GetAllMovies } from "../../api/movie";
+import MovieForm from "./MovieForm";
+import DeleteMovieModal from "./DeleteMovieModal";
 
 const MovieList = () => {
-  const [movies, setMovies] = React.useState([]);
+  const [movies, setMovies] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
   const dispatch = useDispatch();
   const getData = async () => {
     try {
       dispatch(showLoading());
       const response = await GetAllMovies();
-      const allMovies = response?.data;
+      const allMovies = response?.data.map((movie) => ({
+        ...movie,
+        key: movie._id,
+      }));
       setMovies(allMovies);
       dispatch(hideLoading());
     } catch (e) {
@@ -72,12 +32,112 @@ const MovieList = () => {
     }
   };
 
+  const tableHeading = [
+    {
+      title: "Poster",
+      dataIndex: "poster",
+      key: "poster",
+      render: (text, data) => (
+        <img
+          width="75"
+          height="115"
+          style={{ objectFit: "cover" }}
+          src={data?.poster}
+          alt="poster"
+        />
+      ),
+    },
+    {
+      title: "Movie Name",
+      dataIndex: "movieName",
+      key: "movieName",
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Duration",
+      dataIndex: "duration",
+      key: "duration",
+      render: (text) => `${text} min`,
+    },
+    {
+      title: "Genre",
+      dataIndex: "genre",
+      key: "genre",
+    },
+    {
+      title: "Language",
+      dataIndex: "language",
+      key: "language",
+    },
+    {
+      title: "Release Date",
+      dataIndex: "releaseDate",
+      key: "releaseDate",
+      render: (text, data) => moment(data.releaseDate).format("MM-DD-YYYY"),
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (text, data) => (
+        <div>
+          <Button
+            onClick={() => {
+              setIsModalOpen(true);
+              setSelectedMovie(data);
+            }}
+          >
+            <EditOutlined />
+          </Button>
+          <Button
+            onClick={() => {
+              setIsDeleteModalOpen(true);
+              setSelectedMovie(data);
+            }}
+          >
+            <DeleteOutlined />
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
   useEffect(() => {
     getData();
   }, []);
   return (
     <div>
+      <div className="d-flex justify-content-end">
+        <Button
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          Add Movie
+        </Button>
+      </div>
       <Table columns={tableHeading} dataSource={movies} />
+      {isModalOpen && (
+        <MovieForm
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          selectedMovie={selectedMovie}
+          setSelectedMovie={setSelectedMovie}
+          getData={getData}
+        />
+      )}
+      {isDeleteModalOpen && (
+        <DeleteMovieModal
+          isDeleteModalOpen={isDeleteModalOpen}
+          selectedMovie={selectedMovie}
+          setIsDeleteModalOpen={setIsDeleteModalOpen}
+          setSelectedMovie={setSelectedMovie}
+          getData={getData}
+        />
+      )}
     </div>
   );
 };
