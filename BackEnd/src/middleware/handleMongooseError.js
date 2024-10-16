2f0380c3b46f131be2797c037d31d749e9d222f9
Kunal19880711 <kunal21634267+github@gmail.com>
2024-10-16 17:56:23 +0530

    [BackEnd]: fix mongooseError handling

--- BackEnd/src/middleware/handleMongooseError.js
+++ BackEnd/src/middleware/handleMongooseError.js
@@ -1,7 +1,11 @@
+const mongoose 
 const HttpError = require("../common/HttpError");
 
 module.exports = function handleMongooseError(error, req, res, next) {
-  if (error instanceof mongoose.Error.ValidationError) {
+  if (
+    error instanceof mongoose.Error.ValidationError ||
+    error instanceof mongoose.Error.CastError
+  ) {
     const data = Object.keys(err.errors).map((key) => ({
       keyPath: key,
       value: err.errors[key].message,
@@ -9,5 +13,10 @@ module.exports = function handleMongooseError(error, req, res, next) {
     return next(new HttpError(400, "Validation Error", data));
   }
 
+  if (error instanceof mongoose.Error.MongooseError) {
+    console.error(error);
+    return next(new HttpError(500));
+  }
+  
   next(error);
 };
