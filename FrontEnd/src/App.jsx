425da60bd5b14a1a9934bc2850e9e99d60e679b6
Kunal19880711 <kunal21634267+github@gmail.com>
2024-10-24 20:13:46 +0530

    [FrontEnd]: Adding loader

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import Paths, { SubPaths } from "./constants/Paths";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import store from "./redux/store";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Admin from "./pages/Admin";
 import Profile from "./pages/Profile";
@@ -19,6 +18,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 
 function App() {
+  const { loading } = useSelector((store) => store.loader);
   const singleMoviePath = Paths.SingleMovie.replace(
     SubPaths.IdParamFormat,
     SubPaths.IdParam
@@ -29,94 +29,97 @@ function App() {
   );
   return (
     <>
-      <Provider store={store}>
-        <UserSession>
-          <BrowserRouter>
-            <Routes>
-              <Route
-                path={Paths.Home}
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={Paths.Admin}
-                element={
-                  <ProtectedRoute>
-                    <Admin />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={Paths.Profile}
-                element={
-                  <ProtectedRoute>
-                    <Profile />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={Paths.Partner}
-                element={
-                  <ProtectedRoute>
-                    <Partner />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={singleMoviePath}
-                element={
-                  <ProtectedRoute>
-                    <SingleMovie />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={bookShowPath}
-                element={
-                  <ProtectedRoute>
-                    <BookShow />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path={Paths.Login}
-                element={
-                  <NotLoggedIn>
-                    <Login />
-                  </NotLoggedIn>
-                }
-              />
-              <Route
-                path={Paths.Register}
-                element={
-                  <NotLoggedIn>
-                    <Register />
-                  </NotLoggedIn>
-                }
-              />
-              <Route
-                path={Paths.ForgotPassword}
-                element={
-                  <NotLoggedIn>
-                    <ForgotPassword />
-                  </NotLoggedIn>
-                }
-              />
-              <Route
-                path={Paths.ResetPassword}
-                element={
-                  <NotLoggedIn>
-                    <ResetPassword />
-                  </NotLoggedIn>
-                }
-              />
-            </Routes>
-          </BrowserRouter>
-        </UserSession>
-      </Provider>
+      <UserSession>
+        <BrowserRouter>
+          {loading && (
+            <div className="loader-container">
+              <div className="loader"></div>
+            </div>
+          )}
+          <Routes>
+            <Route
+              path={Paths.Home}
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={Paths.Admin}
+              element={
+                <ProtectedRoute>
+                  <Admin />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={Paths.Profile}
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={Paths.Partner}
+              element={
+                <ProtectedRoute>
+                  <Partner />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={singleMoviePath}
+              element={
+                <ProtectedRoute>
+                  <SingleMovie />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={bookShowPath}
+              element={
+                <ProtectedRoute>
+                  <BookShow />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path={Paths.Login}
+              element={
+                <NotLoggedIn>
+                  <Login />
+                </NotLoggedIn>
+              }
+            />
+            <Route
+              path={Paths.Register}
+              element={
+                <NotLoggedIn>
+                  <Register />
+                </NotLoggedIn>
+              }
+            />
+            <Route
+              path={Paths.ForgotPassword}
+              element={
+                <NotLoggedIn>
+                  <ForgotPassword />
+                </NotLoggedIn>
+              }
+            />
+            <Route
+              path={Paths.ResetPassword}
+              element={
+                <NotLoggedIn>
+                  <ResetPassword />
+                </NotLoggedIn>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </UserSession>
     </>
   );
 }
