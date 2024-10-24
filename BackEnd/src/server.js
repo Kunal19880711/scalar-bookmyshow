e2e0c60c943a17187d27b4cb530c4bd23dfa6e0c
Kunal19880711 <kunal21634267+github@gmail.com>
2024-10-25 01:38:39 +0530

    [BackEnd]: relax for stripe

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -33,10 +33,11 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"], // Allow scripts from 'self'
+      frameSrc: ["'self'", "*.stripe.com"],
+      scriptSrc: ["'self'", "*.stripe.com"], // Allow scripts from 'self'
       styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles (unsafe)
       imgSrc: ["'self'", "data:", "*"], // Allow images from 'self' and data URLs,
-      connectSrc: ["'self'"], // Allow connections to 'self'
+      connectSrc: ["'self'", "*.stripe.com"], // Allow connections to 'self'
       fontSrc: ["'self'", "fonts.gstatic.com"], // Allow fonts from 'self' and fonts.gstatic.com
       objectSrc: ["'none'"], // Disallow object, embed, and applet elements
       upgradeInsecureRequests: [], // Upgrade insecure requests to HTTPS
