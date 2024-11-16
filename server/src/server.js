0bb18b6c2b997bf482e973c399ae2fc39ec97176
Kunal19880711 <kunal21634267+github@gmail.com>
2024-11-16 11:11:03 +0530

    chore: renaming FrontEnd and BackEnd
    Renamed FrontEnd to client and BackEnd to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,4 @@
 require("dotenv").config({ path: "../.env" });
-console.log(process.env.MONGODB_URL);
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
@@ -49,9 +48,22 @@ app.use(cors()); // TODO: furthur exploration
 app.use(express.json());
 app.use(mongoSanitize());
 app.use("/bms", apiLimiter);
+
+// Serve static files with custom Cache-Control headers
+app.use((req, res, next) => {
+  if (req.url.startsWith("/bms")) {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // No caching for /bms URLs
+  } else if (req.url.match(/\.(css|js|png|jpg|jpeg|gif)$/)) {
+    res.setHeader("Cache-Control", "public, max-age=31536000"); // 1 year
+  } else if (req.url === "/index.html") {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // No caching for index.html
+  }
+  next();
+});
+
 app.use(
   "/",
-  express.static(path.join(__dirname, "..", "..", "FrontEnd", "dist"))
+  express.static(path.join(__dirname, "..", "..", "client", "dist"))
 );
 if (process.env.SESSION_COOKIE_NAME) {
   app.use(cookieParser());
