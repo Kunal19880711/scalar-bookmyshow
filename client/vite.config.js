7135914e9daf3f57028687dca585a36a3f5a2c6e
Kunal19880711 <kunal21634267+github@gmail.com>
2024-11-17 00:46:39 +0530

    refactor: vite build

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,18 @@ dotenv.config({ path: "../.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id, { getModuleInfo, getModuleIds }) {
+          console.log("[id]", id);
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
   plugins: [react()],
   server: {
     proxy: {
