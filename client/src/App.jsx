5a4bb42a7fd3e6fc05c022842c5eb5c1e18d8ea1
Kunal19880711 <kunal21634267+github@gmail.com>
2024-11-17 02:03:28 +0530

    feat: add chuncking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
 import "./App.css";
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider, useSelector } from "react-redux";
 import Paths, { SubPaths } from "./constants/Paths";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Admin from "./pages/Admin";
-import Profile from "./pages/Profile";
-import Partner from "./pages/Partner";
-import UserSession from "./components/UserSession";
 import NotLoggedIn from "./components/NotLoggedIn";
-import SingleMovie from "./pages/SingleMovie";
-import BookShow from "./pages/BookShow";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
+
+const UserSession = lazy(() => import("./components/UserSession"));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const Admin = lazy(() => import("./pages/Admin"));
+const Profile = lazy(() => import("./pages/Profile"));
+const Partner = lazy(() => import("./pages/Partner"));
+const SingleMovie = lazy(() => import("./pages/SingleMovie"));
+const BookShow = lazy(() => import("./pages/BookShow"));
+const ForgotPassword = lazy(() => import("./pages/ForgotPassword"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword"));
 
 function App() {
   const { loading } = useSelector((store) => store.loader);
@@ -27,99 +28,103 @@ function App() {
     SubPaths.IdParamFormat,
     SubPaths.IdParam
   );
+  const loader = (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
   return (
     <>
-      <UserSession>
-        <BrowserRouter>
-          {loading && (
-            <div className="loader-container">
-              <div className="loader"></div>
-            </div>
-          )}
-          <Routes>
-            <Route
-              path={Paths.Home}
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={Paths.Admin}
-              element={
-                <ProtectedRoute>
-                  <Admin />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={Paths.Profile}
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={Paths.Partner}
-              element={
-                <ProtectedRoute>
-                  <Partner />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={singleMoviePath}
-              element={
-                <ProtectedRoute>
-                  <SingleMovie />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={bookShowPath}
-              element={
-                <ProtectedRoute>
-                  <BookShow />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={Paths.Login}
-              element={
-                <NotLoggedIn>
-                  <Login />
-                </NotLoggedIn>
-              }
-            />
-            <Route
-              path={Paths.Register}
-              element={
-                <NotLoggedIn>
-                  <Register />
-                </NotLoggedIn>
-              }
-            />
-            <Route
-              path={Paths.ForgotPassword}
-              element={
-                <NotLoggedIn>
-                  <ForgotPassword />
-                </NotLoggedIn>
-              }
-            />
-            <Route
-              path={Paths.ResetPassword}
-              element={
-                <NotLoggedIn>
-                  <ResetPassword />
-                </NotLoggedIn>
-              }
-            />
-          </Routes>
-        </BrowserRouter>
-      </UserSession>
+      <Suspense fallback={loader}>
+        <UserSession>
+          <BrowserRouter>
+            {loading && loader}
+
+            <Routes>
+              <Route
+                path={Paths.Home}
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={Paths.Admin}
+                element={
+                  <ProtectedRoute>
+                    <Admin />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={Paths.Profile}
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={Paths.Partner}
+                element={
+                  <ProtectedRoute>
+                    <Partner />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={singleMoviePath}
+                element={
+                  <ProtectedRoute>
+                    <SingleMovie />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={bookShowPath}
+                element={
+                  <ProtectedRoute>
+                    <BookShow />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={Paths.Login}
+                element={
+                  <NotLoggedIn>
+                    <Login />
+                  </NotLoggedIn>
+                }
+              />
+              <Route
+                path={Paths.Register}
+                element={
+                  <NotLoggedIn>
+                    <Register />
+                  </NotLoggedIn>
+                }
+              />
+              <Route
+                path={Paths.ForgotPassword}
+                element={
+                  <NotLoggedIn>
+                    <ForgotPassword />
+                  </NotLoggedIn>
+                }
+              />
+              <Route
+                path={Paths.ResetPassword}
+                element={
+                  <NotLoggedIn>
+                    <ResetPassword />
+                  </NotLoggedIn>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </UserSession>
+      </Suspense>
     </>
   );
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import { v6 as uuidv6 } from "uuid";
 import dotenv from "dotenv";
 
 // Load environment variables
 dotenv.config({ path: "../.env" });
 
+const uniqueId = uuidv6();
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    visualizer({
+      // open: true, // Opens the report in the browser
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id, { getModuleInfo, getModuleIds }) {
-          console.log("[id]", id);
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
+        // Use UUID for JavaScript/Chunk file names
+        chunkFileNames: `assets/js/${uniqueId}-[name].js`,
+        entryFileNames: `assets/js/${uniqueId}-[name].js`,
+        // Use UUID for other assets (e.g., images, fonts, CSS)
+        assetFileNames: (assetInfo) => {
+          // Handle different asset types (images, fonts, CSS, etc.)
+          const ext = assetInfo.name?.split(".").pop();
+          return `assets/${ext}/${uniqueId}-[name].[ext]`;
         },
       },
     },
   },
-  plugins: [react()],
   server: {
     proxy: {
       "/bms": {
